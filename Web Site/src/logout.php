239cef769631d526257f94a62fea3aaaedba4af4
victor333simas <victor333simas@gmail.com>
2019-11-03 12:54:16 -0300

    Unity Game Only

--- Web Site/src/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(isset($_SESSION["name"])){
-        unset($_SESSION["name"]);
-        unset($_SESSION["id"]);	
-        header("location ../prime.html");	
-    }
-    header("location: ../contact.html");
-?>
